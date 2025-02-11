ea3be74167c03d927f36c2c775e9b08d7867d1cb
ChrisRL2311 <carosales@vlacademy.com>
2025-02-10 20:38:46 -0600

    error tabla

--- JS/PagesJS/API_Listado_Clientes.js
+++ JS/PagesJS/API_Listado_Clientes.js
@@ -3,20 +3,16 @@ console.log('Funcionando Archivo: API_Listado_Clientes');
 const Api_Usuarios = "https://jsonplaceholder.typicode.com/users"
 
 
-let Datos_Locales = []
-   
-
-
 let DatosFiltrados = []
 let contenedor = document.getElementById('tbody');
 
-DatosFiltrados = Datos_Locales
+
 
 document.addEventListener('DOMContentLoaded',() => {
     consultarDatos()
 });
 
-
+//consulta de api 
 function consultarDatos() {
 
     fetch(Api_Usuarios)
@@ -25,11 +21,32 @@ function consultarDatos() {
         .then(result => {
             // custom error
             console.log(result)
+            DatosFiltrados = result
         })
         .catch(error => {
             // common error
+            alert ("Error al consultar la API: " + error.message)
             return null;
         });
 
 
+}
+
+function Mostrar_CampoTabla() {
+    
+    let html = ""
+
+    Datos_Locales.forEach((element) => {
+
+        console.log(element);
+
+        html += ` 
+        <tr>
+            <td> ${element.id} </td>
+            <td> ${element.name} </td>
+            <td> ${element.email} </td>
+         </tr>
+        
+        `
+});
 }
