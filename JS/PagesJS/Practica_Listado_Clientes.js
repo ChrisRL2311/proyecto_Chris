a7c2ce559610283dea5a4d261477f7815d5c877d
ChrisRL2311 <carosales@vlacademy.com>
2025-02-10 19:54:44 -0600

    Laboratorio 2

--- JS/PagesJS/Practica_Listado_Clientes.js
+++ JS/PagesJS/Practica_Listado_Clientes.js
@@ -1,6 +1,51 @@
 console.log(`Funcionando Archivo:  Practica_Listado_Clientes`);
 
 
+// Datos locales simulando la API
+let Datos_Locales = [
+    { id: 1, name: "Juan Pérez", email: "juan.perez@example.com" },
+    { id: 2, name: "María Gómez", email: "maria.gomez@example.com" },
+    { id: 3, name: "Carlos Rodríguez", email: "carlos.rodriguez@example.com" },
+    { id: 4, name: "Ana Fernández", email: "ana.fernandez@example.com" }
+];
+
+
+let DatosFiltrados = []
+let contenedor = document.getElementById('tbody');
+
+DatosFiltrados = Datos_Locales
+
+document.addEventListener('DOMContentLoaded',() => {
+
+});
+
+function Mostrar_CampoTabla() {}
+    
+    let html = ""
+
+    Datos_Locales.forEach((element) => {
+
+        console.log(element);
+
+        html += ` 
+        <tr>
+            <td> ${element.name} </td>
+            <td> ${element.email} </td>
+            <td> ${element.id} </td>
+         </tr>
+        
+        `
+});
+
+
+
+
+
+contenedor.innerHTML = html
+
+
+
+
 function exportToExcel() {
 
     try {
